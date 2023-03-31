8ba7ba489e7683586ab44240cb6d7b80ac775151
Moses <lazaromoses77@gmail.com>
2023-03-31 04:05:49 +0300

    Adding production env

--- backend/app.js
+++ backend/app.js
@@ -11,3 +11,10 @@ app.use(express.json({ extended: false }));
 app.use('/api/pies', pies)
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server Listeining on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
