60de6dbaae707c5387ba8060189c9bd7c6e6a834
Moses <lazaromoses77@gmail.com>
2023-03-31 04:10:13 +0300

    changing directoty for production env

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.listen(port, () => console.log(`Server Listeining on port ${port}`));
 
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static('client/build'));
+    app.use(express.static('frontend/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
